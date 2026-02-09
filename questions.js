cff4b8200a041d9296fde3d11c77fcf18647f6fd
JAIDEV V <jaidevjaidev548@gmail.com>
2026-02-09 16:06:02 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+const questions = [
+{
+question:"HTML stands for?",
+options:["Hyper Text Markup Language","High Text Machine Language","Hyperlinks Text Mark Language","None"],
+answer:0
+},
+{
+question:"CSS is used for?",
+options:["Styling","Programming","Database","Server"],
+answer:0
+},
+{
+question:"JavaScript is?",
+options:["Programming Language","Markup Language","Database","Browser"],
+answer:0
+},
+{
+question:"JS stands for?",
+options:["javasignal","javascript","javasignal","none"],
+answer:1
+},
+{
+question:"capital of india?",
+options:["delhi","bangalore","kolar","mulbagal"],
+answer:0
+},
+{
+question:"capital of karnataka?",
+options:["bangalore","malur","kgf","bangarapet"],
+answer:0
+},
+{
+question:"name the fruit starts with alphabet A?",
+options:["mango","banana","pineapple","apple"],
+answer:3
+},
+{
+question:"who is the prime minister of india?",
+options:["jaidev","narendra modi","manoj","srivanth"],
+answer:1
+},
+{
+question:"who is the chief minister of karnataka?",
+options:["diwkar","ambrish","siddaramaiah","D K Ravi"],
+answer:2
+},
+{
+question:"who is the founfer of microsoft?",
+options:["bill Gates","nirmala sitaraman","DSP","none"],
+answer:0
+},
+{
+question:"which is the largest city in india?",
+options:["bangalore","mumbai","kolkata","delhi"],
+answer:1
+},
+{
+question:"who invented internet?",
+options:["tim berners lee","siraj","mohan","none"],
+answer:0
+}
+];
